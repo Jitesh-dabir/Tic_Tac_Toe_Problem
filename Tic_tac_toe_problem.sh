fcc8bafba16c5c4ed3d5444afee411c8070981cb
Jitesh-Dabir <jitudabir9@gmail.com>
2020-03-03 12:20:45 +0530

    ADDED:Player would like to start with resetting the board

--- Tic_tac_toe_problem.sh
+++ Tic_tac_toe_problem.sh
@@ -2,3 +2,41 @@
 
 #WELCOME TO TIC TAC TOE PROBLEM MESSAGE
 echo ".......................................Welcome to Tic Tac Toe Problem........................................"
+
+#DECLARE A DICTIONARY
+declare -A gameBOARD
+
+#CONSTANT
+SIZE_OF_BOARD=3
+
+#FUNCTION TO DISPLAY BOARD
+function display()
+{
+	echo 
+	echo "_________________________"
+	for ((rows=0; rows<$SIZE_OF_BOARD; rows++))
+	do
+		for ((cols=0; cols<$SIZE_OF_BOARD; cols++))
+		do
+			echo -ne "${gameBOARD[$rows,$cols]}\t|"
+		done
+		echo
+		echo "_________________________"
+	done
+}
+
+#GENERATE BOARD
+function resetBoard()
+{
+for ((rows=0; rows<$SIZE_OF_BOARD; rows++))
+do
+	for ((cols=0; cols<$SIZE_OF_BOARD; cols++))
+	do
+		gameBOARD[$rows,$cols]=""
+	done
+done
+}
+
+#FUNCTION CALL TO DISPLAY BOARD
+resetBoard
+display
